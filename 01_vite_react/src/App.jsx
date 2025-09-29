bf6b8d2157fd4cb8b92e5c173a7898a5a268225a
Sanky2104 <guptasanskriti21@gmail.com>
2025-09-29 09:41:32 +0530

    creating react app though npx and vite

---
+++ 01_vite_react/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hey, this is Vite from react - Sans</h1>
+  )
+}
+
+export default App
---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>react yt series</h1>
+  );
+}
+
+export default App;
