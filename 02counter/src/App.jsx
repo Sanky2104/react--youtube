4b26cb8e0dcab618072d9465c720413d79ce7432
Sanky2104 <guptasanskriti21@gmail.com>
2025-09-30 10:02:38 +0530

    hooks

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -8,13 +8,26 @@ function App() {
   // let counter = 0
   const addVal = () => {
     // counter++
-    countChangeKaro(counter+1)
+    if (counter < 20){
+      countChangeKaro(counter + 1)
+    } else {
+      alert("Cant go more than 20")
+    }
     // console.log('clicked', counter); // shows old value, because countChangeKaro function is asynchonous
   } 
 
   const removeVal = () => {
-    countChangeKaro(counter--)
+    if (counter > 0){
+      countChangeKaro(counter - 1)
+    } else {
+      alert('Cant go negative')
+    }
   }
+
+  const resetVal = () => {
+    countChangeKaro(0)
+  }
+
   return (
     <>
       <h1>Chai aur react yt series</h1>
@@ -23,6 +36,9 @@ function App() {
       <br />
       <br />
       <button onClick={removeVal}>decrease value, curr val {counter}</button>
+      <br />
+      <br />
+      <button onClick={resetVal}>Reset counter, curernt val: {counter}</button>
     </>
   )
 }
