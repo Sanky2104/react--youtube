ab4eea14fa0b45c8a85be377b6bfaba5bbb722b5
Sanky2104 <guptasanskriti21@gmail.com>
2025-10-10 09:18:09 +0530

    completed password generator

--- 05_password_generator/src/App.jsx
+++ 05_password_generator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 
 function App() {
@@ -7,17 +7,33 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState()
 
+  // useRef hook
+  // useRef ko use karne ke liye 1 variable banana padta hai
+  const passwordRef = useRef(null)
+
   const passwordGenerator = useCallback(() => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if (numberAllowed) str += "0123456789"
     if (charAllowed) str += "!@#$%^&*(){}~`|\?<>_=+'"
-    for (let i = 1; i <= length.length; i++) {
-      let char = Math.floor(Math.random() * str.length() + 1)
-      pass = str.charAt(char)
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
     }
     setPassword(pass)
   }, [length, numberAllowed, charAllowed, setPassword])
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0,length)
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+  
+  // passwordGenerator() // this is the wrong way, we should use useEffect hook
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
+  
   return (
     <>
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-400'>
@@ -29,8 +45,12 @@ function App() {
             className='outline-none w-full py-1 px-3 text-gray-700'
             placeholder='Password'
             readOnly
+            ref={passwordRef}
           />
-          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>
+          <button
+            className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+            onClick={copyPasswordToClipboard}
+          >
             Copy
           </button>
         </div>
